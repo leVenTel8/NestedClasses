e67133eee569833d969d83ecaf4ef925acce0b9c
LeVenTel8 <leventel8@yandex.ru>
2018-12-24 19:58:12 +0300

    анонимный класс создание опарации через интерфейс, добавление реализации методов интерфейса и AccountOperation и добавление записи в ArrayList при вызове методов put и get

---
+++ src/com/company/AccountOperation.java
@@ -0,0 +1,8 @@
+package com.company;
+
+import java.util.Date;
+
+public interface AccountOperation {
+    Date getDate();
+    int amount();
+}
--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 public class Bank {
@@ -8,6 +10,8 @@ public class Bank {
         private String id;
         private String name;
         private int money;
+        private ArrayList<AccountOperation> operations =
+                new ArrayList<AccountOperation>();
 
         public Account(String id, String name, int amount){
             this.id = id; //this ссылка на самого себя
@@ -44,7 +48,17 @@ public class Bank {
             return;
         }
 
-        account.money += amount;
+        AccountOperation operation = new AccountOperation() {
+            public Date getDate() {
+                return new Date();
+            }
+
+            public int amount() {
+                return amount;
+            }
+        };
+        account.operations.add(operation);
+
     }
 
     public void getMoney(String id, int amount){
@@ -53,6 +67,16 @@ public class Bank {
             return;
         }
 
-        account.money -= amount;
+        AccountOperation operation = new AccountOperation() {
+            public Date getDate() {
+                return new Date();
+            }
+
+            public int amount() {
+                return -amount;
+            }
+        };
+        account.operations.add(operation);
+
     }
 }
